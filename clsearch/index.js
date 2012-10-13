d35b1850e52e7a900ca4b342e7f6617065762816
William Trevor Olson <wtolson@gmail.com>
2012-10-13 15:55:26 -0700

    Make madlibs for craiglist!

--- clsearch/index.js
+++ clsearch/index.js
@@ -1,5 +1,6 @@
 var http = require('http')
-  , cheerio = require('cheerio');
+  , cheerio = require('cheerio')
+  , pos = require('pos');
 
 function loadBody(resp, callback) {
   resp.setEncoding('utf8')
@@ -41,8 +42,120 @@ function parsePage(body, callback) {
     parseText(title, body, callback)
 }
 
+
+var __sentenceEnd = {
+  '.': true,
+  '!': true,
+  '?': true
+};
+
+function splitSentences(taggedWords) {
+  var sentences = []
+    , current = []
+    , word
+    , i;
+
+  for (i = 0; i < taggedWords.length; i++) {
+    word = taggedWords[i]
+    if (__sentenceEnd[word[1]] && current.length) {
+      sentences.push(current)
+      current = []
+    } else {
+      current.push(word)
+    }
+  }
+
+  if (current.length) {
+    sentences.push(current)
+  }
+
+  return sentences
+}
+
+function splitChunks(sentences) {
+  var chunks = []
+    , current = []
+    , i, j, sentence;
+
+  for (i = 0; i < sentences.length; i++) {
+    sentence = sentences[i];
+
+    for (j = 0; j < sentence.length; j++) {
+      current.push(sentence[j])
+    }
+
+    if (current.length >= 10) {
+      chunks.push(current)
+      current = []
+    }
+  }
+
+  if (current.length) {
+    chunks.push(current)
+  }
+
+  return chunks
+}
+
+var __pos = {
+  'JJ': 'Adjective',
+  'NN': 'Noun',
+  'NNP': 'Proper noun',
+  'VB': 'Verb'
+};
+
+function generateMadlib(chunk) {
+  var i, word
+  , inputs = 0
+  , madlib = []
+  , current = [];
+
+  while (inputs < 3) {
+    i =  0^(Math.random()*chunk.length)
+    word = chunk[i]
+
+    if (word.pos) {
+      continue
+    }
+
+    word.pos = __pos[word[1]]
+    if (word.pos) {
+      inputs++
+    }
+  }
+
+  for (i = 0; i < chunk.length; i++) {
+    word = chunk[i]
+    if (word.pos) {
+      if (current.length) {
+        madlib.push(current.join(' '))
+        current = []
+      }
+      madlib.push({type: word.pos})
+    } else {
+      current.push(word[0])
+    }
+  }
+  if (current.length) {
+    madlib.push(current.join(' '))
+    current = []
+  }
+
+  return madlib
+}
+
 function parseText(title, body, callback) {
-  callback(title, body)
+  var words = new pos.Lexer().lex(body)
+    , taggedWords = new pos.Tagger().tag(words)
+    , sentences = splitSentences(taggedWords)
+    , chunks = splitChunks(sentences)
+    , madlibs = [], i;
+
+  for (i=0; i<chunks.length; i++) {
+    madlibs.push(generateMadlib(chunks[i]))
+  }
+
+  callback(null, title, madlibs)
 }
 
 getMadLib = function(query, callback){
@@ -63,8 +176,11 @@ getMadLib = function(query, callback){
 
 exports.getMadLib = getMadLib;
 
-
-// getMadLib('play', function(title, body) {
+// getMadLib('fun', function(title, body, madlibs) {
 //   console.log(title)
 //   console.log(body)
+
+//   for (var i=0; i<madlibs.length; i++) {
+//   console.log(JSON.stringify(madlibs[i]))
+//   }
 // })
