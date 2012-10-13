67bdf2df25d6beeceee223acc35a40d729399a9b
Spencer Alger <spencer@spenceralger.com>
2012-10-13 16:16:06 -0700

    fleshing out the socket communication

--- collab.js
+++ collab.js
@@ -1,45 +1,65 @@
 
 var clone = require('clone');
 
+var log = require('util').log;
+
 var newStory = (function(){
     var library = [
-        [],
-        [],
-        []
+        [], [], []
     ];
 
-    var i = 0;
+    var i = -1;
 
     return function ns (){
+        log('sending story '+(i+1))
         return clone(library[++i % library.length], false);
     }
 })()
 
-var archive = [],
-    activeCollab = new Collab();
-
 function Collab (story){
+    log('new collab');
     this.story = newStory();
     this.blocksSent = 0;
 }
 
+Collab.prototype = {
+    nextSentence: function(){
+        var s
+        if (this.blocksSent >= this.story.length){
+            log('all of the sentences have been sent')
+            s = false;
+        } else {
+            log('sending sentence '+this.blocksSent)
+            s = this.story[this.blocksSent]
+            log(s)
+            this.blocksSent++;
+        }
+        return s;
+    }
+}
+
 var onComplete = function(){};
+
+var archive = [],
+    activeCollab = new Collab()
 
 module.exports = {
     setCompleteCB: function(cb){
+        log('story complete')
         onComplete = cb;
     },
     getSentence: function(){
-        return activeCollab.nextSentence();
+        return activeCollab.nextSentence()
     },
     saveSentence: function(data, user){
-        activeCollab.story.blocks[data.id][1].value = data.value;
-        activeCollab.story.blocks[data.id][1].value = data.user;
-        activeCollab.blockComplete++;
-        if (activeCollab.blockComplete == activeCollab.story.length){
-            onComplete(activeCollab.story);
+        activeCollab.story[data.id][1].value = data.value
+        activeCollab.story[data.id][1].value = data.user
+        activeCollab.blockComplete++
+        if (activeCollab.blockComplete == activeCollab.story.length) {
+            onComplete(activeCollab.story)
         }
-        archive.push(activeCollab.story);
-        activeCollab = new Collab();
-    }
+        archive.push(activeCollab.story)
+        activeCollab = new Collab()
+    },
+    
 }
---
+++ public/javascripts/main.js
@@ -0,0 +1,47 @@
+var socket = io.connect('http://localhost');
+    socket.on('dontknowyou', function (data) {
+      console.log('What is your name?');
+      socket.emit('meetme', {name: 'Johnny'});
+    });
+
+    socket.on('story-complete', function(story){
+        console.log('this is the story', story);
+        /*
+            [
+                [   // sentence
+                    'I want',
+                    {
+                        type: 'noun',
+                        value: 'pics',
+                        user: 'Johhny'
+                    },
+                    'in my mouth.'
+                ],
+                [
+                    'I want',
+                    {
+                        type: 'noun',
+                        value: 'pics',
+                        user: 'Johhny'
+                    },
+                    'in my mouth.'
+                ],
+                ...
+            ]
+        */
+    })
+
+    // get sentence
+    socket.emit('need-sentence');
+    socket.on('new-sentence', function(sentence){
+
+    })
+
+    // parts filled
+    submitSentence = function(){
+        socket.emit('usethis', function(){
+
+        })
+    }
+
+    
